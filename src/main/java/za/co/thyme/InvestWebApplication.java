f6b48f6095a11fe493d898450c68f9b19a69ed11
theunisendroom <theunisg.vdm@gmail.com>
2016-11-23 01:44:15 +0200

    First thyme invest web commit

---
+++ src/main/java/za/co/thyme/InvestWebApplication.java
@@ -0,0 +1,52 @@
+package za.co.thyme;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class InvestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestWebApplication.class, args);
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+	
+	@RequestMapping("/token")
+	public Map<String, String> token(HttpSession session) {
+		return Collections.singletonMap("token", session.getId());
+	}
+	
+	@Configuration
+	  @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	    	http
+	        .httpBasic().and()
+	        .authorizeRequests()
+	          .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
+	          .authenticated().and()
+	        .csrf()
+	          .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	    }
+	  }
+}
